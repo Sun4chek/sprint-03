a4cfa3e7c6c8a6fd8b7bc50047980cd814418597
Avonic <boysfromhell@mail.ru>
2025-01-04 18:36:17 +0300

    Задание выпролено добавлен счетчик

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var plusbutton: UIButton!
+    
+    @IBOutlet weak var counter: UILabel!
+    
+    
+    private var Counter : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        counter.text = String(Counter)
     }
 
-
+    @IBAction func sum(_ sender: Any) {
+        Counter += 1
+        counter.text = String(Counter)
+        
+    }
+    
 }
 
